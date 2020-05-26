d6c11a4ad3990b151d5dd7b8ae4ee3c669a2e44e
Philipp Schuster <phip1611@gmail.com>
2020-05-26 17:07:52 +0200

    with function args instead of harcoded values

--- x86-div-and-remainder-combined/main.c
+++ x86-div-and-remainder-combined/main.c
@@ -5,8 +5,8 @@
 extern int calc(int, int);
 
 int main() {
-    int a = 7;
-    int b = 4;
+    int a = 4;
+    int b = 1;
     int x = calc(a, b);
     printf("calc(%d, %d) = %d\n", a, b, x);
     return 0;
