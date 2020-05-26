262d01a401e6ff329452389d5a9a805a5530a128
Philipp Schuster <phip1611@gmail.com>
2020-05-26 16:40:06 +0200

    WIP

--- x86-div-and-remainder-combined/main.c
+++ x86-div-and-remainder-combined/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-extern int calc();
+// Calculates the sum of the division and the remainder of a and b,
+// i.e. (a / b) + (a % b)
+extern int calc(int, int);
 
 int main() {
-    int x = calc();
-    printf("x = %d\n", x);
+    int a = 7;
+    int b = 4;
+    int x = calc(a, b);
+    printf("calc(%d, %d) = %d\n", a, b, x);
     return 0;
 }
