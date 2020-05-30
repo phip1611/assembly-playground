af174118d388c3bda95eb3e68b8cb6bb8a7c8419
Philipp Schuster <phip1611@gmail.com>
2020-05-30 16:06:45 +0200

    tabs -> spaces

--- x86-div-and-remainder-combined/main.c
+++ x86-div-and-remainder-combined/main.c
@@ -6,15 +6,16 @@
 extern int calc(int, int);
 
 int main(int argc, char ** argv) {
-	if (argc != 3) {
-		printf("Bitte zwei Zahlen übergeben!\n");
-		exit(1);
-	}
-	int input_a = atoi(argv[1]);
-	int input_b = atoi(argv[2]);
+    if (argc != 3) {
+        printf("Bitte zwei Zahlen übergeben!\n");
+        exit(1);
+    }
 	
+    int input_a = atoi(argv[1]);
+    int input_b = atoi(argv[2]);
+
     int x = calc(input_a, input_b);
     printf("calc(%d, %d) = %d\n", input_a, input_b, x);
-    
+
     return 0;
 }
